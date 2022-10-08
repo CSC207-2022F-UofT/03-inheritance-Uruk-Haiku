f32f133c0b916a8636e4fa4bcfbe472365d0e6c8
Platysquid <59447359+Platysquid@users.noreply.github.com>
2022-10-07 22:37:58 -0400

    solutions

--- src/main/java/Bag.java
+++ src/main/java/Bag.java
@@ -14,7 +14,10 @@ public abstract class Bag {
      *       - an array of Strings named contents
      */
 
-
+    private String color;
+    private int numberOfContents;
+    private int capacity;
+    private String[] contents;
 
 
     /*
@@ -27,7 +30,12 @@ public abstract class Bag {
      * its contents.)
      */
 
-
+    public Bag(String color, int capacity) {
+        this.color = color;
+        this.numberOfContents = 0;
+        this.capacity = capacity;
+        this.contents = new String[0];
+    }
 
 
     /*
@@ -38,16 +46,26 @@ public abstract class Bag {
      *           - getCapacity
      */
 
+    public String getColor() {
+        return this.color;
+    }
 
+    public int getNumberOfContents() {
+        return this.numberOfContents;
+    }
+
+    public int getCapacity() {
+        return this.capacity;
+    }
 
 
     /*
      * TODO: Create a setter function called setColor which sets the
      *       color of this bag to the given color.
      */
-
-
-
+    public void setColor(String color) {
+        this.color = color;
+    }
 
 
     /*
@@ -60,9 +78,24 @@ public abstract class Bag {
      *       This method should return true if the item was added
      *       and false otherwise.
      */
+    public boolean addItem(String item) {
+        if (this.numberOfContents >= this.capacity) {
+            return false;
+        }
+        else {
+            String[] newContents = new String[this.contents.length + 1];
 
+            // Awesome Java syntax I just learned about??? From an IntelliJ Suggestion?
+            System.arraycopy(this.contents, 0, newContents, 0, this.contents.length);
 
+            newContents[this.contents.length] = item;
 
+            this.contents = newContents;
+            this.numberOfContents++;
+
+            return true;
+        }
+    }
 
 
     /**
@@ -75,9 +108,18 @@ public abstract class Bag {
      *
      * @return
      */
+    public String popItem() {
+        String[] newContents = new String[this.contents.length - 1];
 
+        System.arraycopy(this.contents, 0, newContents, 0, this.contents.length - 1);
 
+        String target = this.contents[this.contents.length - 1];
 
+        this.contents = newContents;
+        this.numberOfContents--;
+
+        return target;
+    }
 
 
     /**
@@ -87,7 +129,10 @@ public abstract class Bag {
      */
     public void increaseCapacity(int n) {
         // TODO: Implement this method.
-
+        String[] newContents = new String[this.contents.length + n];
+        System.arraycopy(this.contents, 0, newContents, 0, this.contents.length);
+        this.capacity = this.capacity + n;
+        this.contents = newContents;
     }
 
     /**
